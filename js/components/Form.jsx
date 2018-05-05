8639633679af78b40c17c3a5049e388dcecf8522
Fabian Luna <flb031@gmail.com>
2018-05-04 23:35:55 -0500

    [TEST-001] Add style form

--- js/components/Form.jsx
+++ js/components/Form.jsx
@@ -13,25 +13,38 @@ class Form extends Component {
   render() {
     return (
       <form className="Form">
-        <FormGroup>
-          <h4>Entra na minah conta</h4>
-          <InputGroup>
-            <FormControl type="text" placeholder="Email" />
-            <InputGroup.Addon>
-              <Glyphicon glyph="user" />
+        <FormGroup className="m-b-none">
+
+          <h4 className="Form__title m-b">Entra na minah conta</h4>
+
+          <InputGroup className="m-quarter-b Form__group-input">
+            <FormControl
+              className="Form__group-input__input"
+              type="text"
+              placeholder="Email"
+            />
+            <InputGroup.Addon className="Form__group-input__icon-input">
+              <Glyphicon className="Form__group-input__icon" glyph="user" />
             </InputGroup.Addon>
           </InputGroup>
 
-          <InputGroup>
-            <FormControl type="text" placeholder="Senha" />
-            <InputGroup.Addon>
-              <Glyphicon glyph="lock" />
+          <InputGroup className="Form__group-input">
+            <FormControl
+              className="Form__group-input__input"
+              type="text"
+              placeholder="Senha"
+            />
+            <InputGroup.Addon className="Form__group-input__icon-input">
+              <Glyphicon className="Form__group-input__icon" glyph="lock" />
             </InputGroup.Addon>
           </InputGroup>
-          <Checkbox>
+
+          <Checkbox className="Form__check">
             Mantenha-me conectado
           </Checkbox>
-          <Button bsStyle="primary">Entrar</Button>
+
+          <Button className="Form__button" bsStyle="primary">Entrar</Button>
+
         </FormGroup>
       </form>
     );
