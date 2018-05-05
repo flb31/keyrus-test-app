7f01141e709ad5bd36a788ee116a5d181bd794ec
Fabian Luna <flb031@gmail.com>
2018-05-05 11:06:31 -0500

    [TEST-006] Add test for validation helper

--- js/helpers/validations.js
+++ js/helpers/validations.js
@@ -1,15 +1,9 @@
 export function validateEmail(value) {
-  if (!value) {
-    return false;
-  }
-  return value.match(
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
-  );
+  const patt = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g);
+  return patt.test(value);
 }
 
 export function validatePassword(value) {
-  if (!value) {
-    return false;
-  }
-  return value.match(/[\w\W]{8,}/g);
+  const patt = new RegExp(/[\w\W]{8,}/g);
+  return patt.test(value);
 }
